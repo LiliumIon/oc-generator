3d7f4d328895f2daee76490f2f47baf5fffc7348
Lilium Ion <liliumion@gmail.com>
2023-02-07 20:16:04 -0500

    Added more attributes

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
-const randomWord = words => {
-    return words[Math.floor(Math.random() * words.length)];
+const randomWord = num => {
+    return Math.floor(Math.random() * num);
 }
 
 const appearance = {
-    hair: ['black', 'brown', 'blonde', 'red', 'dyed'],
+    hair: ['black', 'brown', 'blonde', 'red', 'green', 'purple', 'blue', 'white'],
     eyes: ['black', 'brown', 'blue', 'green', 'hazel'],
     skin: ['pale', 'medium', 'dark']
 }
-console.log(`Hair Color: ${randomWord(appearance.hair)}
+let array = [];
+for (let prop in appearance) {
+    array.push(appearance[prop][randomWord(appearance[prop].length)]);
+}
+console.log(`Your character has ${array[0]} hair, ${array[1]} eyes, and ${array[2]} skin.`);
+/* console.log(`Hair Color: ${randomWord(appearance.hair)}
 Eye Color: ${randomWord(appearance.eyes)}
-Skin Color: ${randomWord(appearance.skin)}`);
+Skin Color: ${randomWord(appearance.skin)}`); */
